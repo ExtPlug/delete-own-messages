df18512e8e9f54b7196ad695649fda088f26ab04
René Kooi <rene@kooi.me>
2015-08-17 20:41:05 +0200

    implementation

--- src/main.js
+++ src/main.js
@@ -1,17 +1,47 @@
 define(function (require, exports, module) {
 
   const Plugin = require('extplug/Plugin');
+  const Events = require('plug/core/Events');
+  const { deleteChat } = require('plug/facades/chatFacade');
+  const Lang = require('lang/Lang');
+  const $ = require('jquery');
 
   const DeleteOwnMessages = Plugin.extend({
     name: 'Delete Own Messages',
     description: 'Allows you to delete your own messages (...if you\'re staff.)',
 
-    enable() {
-      // code to start your plugin
+    style: {
+      '#chat .deletable:hover .delete-button': {
+        'display': 'block'
+      }
     },
 
-    disable() {
-      // code to undo what you did in enable()
+    enable() {
+      this.listenTo(Events, 'chat:afterreceive', (message, el) => {
+        if (!el || !el.length) return;
+
+        let user = API.getUser();
+        if (user.role >= API.ROLE.BOUNCER &&
+            message.uid &&
+            message.cid &&
+            el.find('.delete-button').length === 0) {
+          let deleteButton = $('<div />')
+            .addClass('delete-button')
+            .on('click', this.deleteChat)
+            .text(Lang.chat.delete);
+
+          el.addClass('deletable').append(deleteButton);
+        }
+      });
+    },
+
+    deleteChat(e) {
+      let button = $(e.currentTarget);
+      let el = button.parent('.deletable');
+      if (el && el.data('cid')) {
+        deleteChat(el.data('cid'));
+      }
+      button.remove();
     }
   });
 
